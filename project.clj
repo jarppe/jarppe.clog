478e967df993d1e22447ed459d3a8265049c755f
jarppe <jarppe@metosin.fi>
2012-09-08 16:39:41 +0300

    Added lazytest: lein midje --lazytest

--- project.clj
+++ project.clj
@@ -3,8 +3,10 @@
   :url "http://github.com/jarppe/jarppe.clog"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [joda-time/joda-time "2.1"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0" :exclusions [org.clojure/clojure]]]
-                   :plugins [[lein-midje "2.0.0-SNAPSHOT"]]}}
+  :profiles {:dev {:dependencies [[midje "1.4.0" :exclusions [org.clojure/clojure]]
+                                  [com.stuartsierra/lazytest "1.2.3"]]
+                   :plugins [[lein-midje "2.0.0-SNAPSHOT"]]
+                   :repositories {"stuart" "http://stuartsierra.com/maven2"}}}
   :warn-on-reflection true
   :main jarppe.clog.core
   :min-lein-version "2.0.0")
