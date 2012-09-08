5e080fd8f6978cdda8f6be369216edd508d1aa92
jarppe <jarppe@metosin.fi>
2012-09-08 16:27:48 +0300

    Initial implementation, no reflections warnings, minimal tests.

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject jarppe.clog "0.0.1-SNAPSHOT"
   :description "jarppe.clog"
   :url "http://github.com/jarppe/jarppe.clog"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0" :exclusions [org.clojure/clojure]]]}}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [joda-time/joda-time "2.1"]]
+  :profiles {:dev {:dependencies [[midje "1.4.0" :exclusions [org.clojure/clojure]]]
+                   :plugins [[lein-midje "2.0.0-SNAPSHOT"]]}}
   :warn-on-reflection true
   :main jarppe.clog.core
   :min-lein-version "2.0.0")
---
+++ test/jarppe/clog_test.clj
@@ -0,0 +1,17 @@
+(ns jarppe.clog-test
+  (:use jarppe.clog
+        clojure.test
+        midje.sweet))
+
+(facts
+  (fact (-> (join-args (StringBuilder.) ["a" "b" "c"]) .toString)  => "abc")
+  (fact (-> (join-args (StringBuilder.) ["a" 42 "c"]) .toString)  => "a42c")
+  (fact (-> (join-args (StringBuilder.) ["a" "b" "c" (Exception. "Oh noes")]) .toString)  => "abc"))
+
+(facts
+  (fact (-> (stacktrace (StringBuilder.) ["a" "b" "c"]) .toString) => "")
+  (fact (-> (stacktrace (StringBuilder.) ["a" "b" "c" (Exception. "Oh noes")]) .toString) => (has-prefix "java.lang.Exception: Oh noes\n")))
+
+(facts
+  (fact (make-log-message "foo" 42 :fatal "a" "b" "c") => (has-prefix "fatal 2012/09/08 16:11:37.592 [foo:42] abc\n")
+        (provided (time-str) => "2012/09/08 16:11:37.592")))
