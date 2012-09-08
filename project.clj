fae3c5279d1dccb0897f10e46e925f21a679cf76
jarppe <jarppe@metosin.fi>
2012-09-08 17:21:43 +0300

    0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jarppe/clog "0.1.0"
+(defproject jarppe/clog "0.1.1-SNAPSHOT"
   :description "jarppe.clog"
   :url "http://github.com/jarppe/jarppe.clog"
   :dependencies [[org.clojure/clojure "1.4.0"]
